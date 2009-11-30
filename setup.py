781981efabfc9a33e018ffa0a2690a2ab112ec0c
Justin Quick <justquick@gmail.com>
2009-11-29 22:32:36 -0500

    ruminations of v0.5

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name = "pygooglevoice",
-    version = '0.4',
+    version = '0.5',
     url = 'http://code.google.com/p/pygooglevoice',
     author = 'Justin Quick and Joe McCall',
     author_email='justquick@gmail.com, joe@mcc4ll.us',
     description = 'Python 2/3 Interface for Google Voice',
     packages = ['googlevoice'],
-    scripts = ['bin/gvoice','bin/asterisk']#,'bin/gvi']
+    scripts = ['bin/gvoice','bin/asterisk-gvoice-setup']#,'bin/gvi']
 )
