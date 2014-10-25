6e05bc15f5687c95f814ff6018952c12225f9480
Jason R. Coombs <jaraco@jaraco.com>
2014-10-25 10:54:54 -0400

    let the logging framework do its thing.

--- googlevoice/voice.py
+++ googlevoice/voice.py
@@ -1,4 +1,5 @@
 import re
+import logging
 import base64
 
 from .conf import config
@@ -12,12 +13,9 @@ import requests
 qpat = re.compile(r'\?')
 
 if settings.DEBUG:
-    import logging
-    logging.basicConfig()
-    log = logging.getLogger('PyGoogleVoice')
-    log.setLevel(logging.DEBUG)
-else:
-    log = None
+    logging.basicConfig(level=logging.DEBUG)
+
+log = logging.getLogger(__name__)
 
 
 class Voice(object):
@@ -264,8 +262,7 @@ class Voice(object):
             data = urllib.parse.urlencode(data)
         headers.update({'User-Agent': 'PyGoogleVoice/0.5'})
         url = getattr(settings, page)
-        if log:
-            log.debug('%s?%s - %s' % (url[22:], data or '', headers))
+        log.debug('%s?%s - %s' % (url[22:], data or '', headers))
         if page in ('DOWNLOAD', 'XML_SEARCH'):
             url = url + data
             data = None
